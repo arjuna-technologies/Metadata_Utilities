2f23e7ee1cf9139898c9020f5b30577e16474a4b
Stuart Wheater <stuart.wheater@arjuna.com>
2014-10-31 16:11:08 +0000

    First complete generater

--- generator-jdbc/src/main/java/com/arjuna/dbutils/metadata/jdbc/generator/JDBCDatabaseMetadataScan.java
+++ generator-jdbc/src/main/java/com/arjuna/dbutils/metadata/jdbc/generator/JDBCDatabaseMetadataScan.java
@@ -42,56 +42,106 @@ public class JDBCDatabaseMetadataScan
             boolean      firstItem = true;
             StringBuffer rdfText   = new StringBuffer();
             rdfText.append("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n\n");
-            rdfText.append("<rdf:RDF xmlns:rdf=\"http://www.w3.org/1999/02/22-rdf-syntax-ns#\" xmlns:rdfs=\"http://www.w3.org/2000/01/rdf-schema#\" xmlns:pg=\"http://rdfs.arjuna.com/jdbc/postgresql#\">\n");
+            rdfText.append("<rdf:RDF xmlns:rdf=\"http://www.w3.org/1999/02/22-rdf-syntax-ns#\" xmlns:rdfs=\"http://www.w3.org/2000/01/rdf-schema#\" xmlns:pg=\"http://rdfs.arjuna.com/jdbc/postgresql#\" xmlns:d=\"http://rdfs.arjuna.com/description#\">\n");
 
             List<UUID> tableUUIDs = new LinkedList<>();
 
-            DatabaseMetaData databaseMetaData   = connection.getMetaData();
-            ResultSet        allTablesResultSet = databaseMetaData.getTables(null, "public", null, null);
-            while (allTablesResultSet.next())
+            DatabaseMetaData databaseMetaData = connection.getMetaData();
+            ResultSet        tablesResultSet  = databaseMetaData.getTables(null, "public", null, null);
+            while (tablesResultSet.next())
             {
-                String tableName    = allTablesResultSet.getString("table_name");
-                String tableType    = allTablesResultSet.getString("table_type");
-                String tableRemarks = allTablesResultSet.getString("remarks");
+                String tableCatalog = tablesResultSet.getString("table_cat");
+                String tableSchema  = tablesResultSet.getString("table_schem");
+                String tableName    = tablesResultSet.getString("table_name");
+                String tableType    = tablesResultSet.getString("table_type");
+                String tableRemarks = tablesResultSet.getString("remarks");
                 if (tableType.equals("TABLE"))
                 {
-                	if (firstItem)
-                		firstItem = false;
-                	else
+                    List<UUID> fieldUUIDs = new LinkedList<>();
+
+                    ResultSet columnsResultSet = databaseMetaData.getColumns(tableCatalog, tableSchema, tableName, null);
+                    while (columnsResultSet.next())
+                    {
+                        String fieldName    = columnsResultSet.getString("COLUMN_NAME");
+                        String fieldType    = columnsResultSet.getString("TYPE_NAME");
+                        String fieldRemarks = columnsResultSet.getString("REMARKS");
+
+                        UUID fieldUUID = UUID.randomUUID();
+                        fieldUUIDs.add(fieldUUID);
+
+                        if (firstItem)
+                            firstItem = false;
+                        else
+                            rdfText.append('\n');
+
+                        rdfText.append("    <pg:Field rdf:about=\"");
+                        rdfText.append(baseRDFURI.resolve('#' + fieldUUID.toString()));
+                        rdfText.append("\">\n");
+                        rdfText.append("        <d:hasTitle>");
+                        rdfText.append(fieldName);
+                        rdfText.append("</d:hasTitle>\n");
+                        if (fieldRemarks != null)
+                        {
+                            rdfText.append("        <d:hasSummary>");
+                            rdfText.append(fieldRemarks);
+                            rdfText.append("</d:hasSummary>\n");
+                        }
+                        rdfText.append("        <pg:hasFieldName>");
+                        rdfText.append(fieldName);
+                        rdfText.append("</pg:hasFieldName>\n");
+                        rdfText.append("        <pg:hasFieldType>");
+                        rdfText.append(fieldType);
+                        rdfText.append("</pg:hasFieldType>\n");
+                        rdfText.append("    </pg:Field>\n");
+                    }
+
+                    UUID tableUUID = UUID.randomUUID();
+                    tableUUIDs.add(tableUUID);
+
+                    if (firstItem)
+                        firstItem = false;
+                    else
                         rdfText.append('\n');
 
-                	UUID tableUUID = UUID.randomUUID();
-                	tableUUIDs.add(tableUUID);
-
-                	rdfText.append("    <pg:Table rdf:about=\"");
+                    rdfText.append("    <pg:Table rdf:about=\"");
                     rdfText.append(baseRDFURI.resolve('#' + tableUUID.toString()));
                     rdfText.append("\">\n");
+                    rdfText.append("        <d:hasTitle>");
+                    rdfText.append(tableName);
+                    rdfText.append("</d:hasTitle>\n");
+                    if (tableRemarks != null)
+                    {
+                        rdfText.append("        <d:hasSummary>");
+                        rdfText.append(tableRemarks);
+                        rdfText.append("</d:hasSummary>\n");
+                    }
                     rdfText.append("        <pg:hasTableName>");
                     rdfText.append(tableName);
                     rdfText.append("</pg:hasTableName>\n");
                     rdfText.append("        <pg:hasTableField>\n            <rdf:Seq>\n");
-                    rdfText.append("TO DO\n");
-                    rdfText.append("            </rdf:Seq>\n       </pg:hasTableField>\n");
-                    rdfText.append("    </pg:Table>\n");
-
-                    ResultSet allColumnsResultSet = databaseMetaData.getColumns(allTablesResultSet.getString("table_cat"), allTablesResultSet.getString("table_schem"), allTablesResultSet.getString("table_name"), null);
-                    while (allColumnsResultSet.next())
+                    for (UUID fieldUUID: fieldUUIDs)
                     {
-                        String columnName     = allColumnsResultSet.getString("COLUMN_NAME");
-                        String columnNRemarks = allColumnsResultSet.getString("REMARKS");
+                        rdfText.append("                <rdf:li rdf:resource=\"");
+                        rdfText.append(baseRDFURI.resolve('#' + fieldUUID.toString()));
+                        rdfText.append("\"/>\n");
                     }
+                    rdfText.append("            </rdf:Seq>\n        </pg:hasTableField>\n");
+                    rdfText.append("    </pg:Table>\n");
                 }
             }
 
-        	if (firstItem)
-        		firstItem = false;
-        	else
+            if (firstItem)
+                firstItem = false;
+            else
                 rdfText.append('\n');
 
-        	rdfText.append("    <pg:Database rdf:about=\"");
+            rdfText.append("    <pg:Database rdf:about=\"");
             rdfText.append(baseRDFURI.resolve('#' + databaseName));
             rdfText.append("\">\n");
-            rdfText.append("        <pg:hasDatabaseType>PostgreSQL</pg:hasDatabaseName>\n");
+            rdfText.append("        <d:hasTitle>");
+            rdfText.append(databaseName);
+            rdfText.append("</d:hasTitle>\n");
+            rdfText.append("        <pg:hasDatabaseType>PostgreSQL</pg:hasDatabaseType>\n");
             rdfText.append("        <pg:hasDatabaseHostName>");
             rdfText.append(databaseServerName);
             rdfText.append("</pg:hasDatabaseHostName>\n");
@@ -110,11 +160,9 @@ public class JDBCDatabaseMetadataScan
             rdfText.append("        <pg:hasDatabaseTable>\n            <rdf:Seq>\n");
             for (UUID tableUUID: tableUUIDs)
             {
-            	rdfText.append("                <rdf:li>\n");
-            	rdfText.append("                    <pg:hasDatabaseTable rdf:resource=\"");
-            	rdfText.append(baseRDFURI.resolve('#' + tableUUID.toString()));
-            	rdfText.append("\"/>\n");
-            	rdfText.append("                </rdf:li>\n");
+                rdfText.append("                <rdf:li rdf:resource=\"");
+                rdfText.append(baseRDFURI.resolve('#' + tableUUID.toString()));
+                rdfText.append("\"/>\n");
             }
             rdfText.append("            </rdf:Seq>\n       </pg:hasDatabaseTable>\n");
             rdfText.append("    </pg:Database>\n");
